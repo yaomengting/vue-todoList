a95d57333c22f1ca88f5b277a2b073c01fe08127
Mengting Yao <mengtingyao@Mengtings-Mini.attlocal.net>
2024-05-13 20:48:09 -0500

    implement add function

--- main.js
+++ main.js
@@ -4,12 +4,21 @@ const app = Vue.createApp({
       todos: [
         {id: 1, title: "Learn Vue"},
         {id: 2, title: "Learn React"},
-      ]
+      ],
+      description: ""
     }
   },
   methods: {
     deleteTodo(id){
       this.todos = this.todos.filter(todo=> todo.id !== id)
+    },
+    onSubmit(){
+      let newTodo = {
+        id: this.description,
+        title: this.description
+      };
+      this.todos.push(newTodo)
+
     }
   }
 })
